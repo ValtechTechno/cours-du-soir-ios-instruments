e2c4dc85d08725e79302f7b5abc722e230e15b56
Sylvain Rousseau <sylrouss@gmail.com>
2012-03-21 09:44:29 +0100

    Made a significant malloc/free in CDSProject with a dummy buffer

--- CoursDuSoirIOS-Instruments/CoursDuSoirIOS-Instruments/CDSProject.m
+++ CoursDuSoirIOS-Instruments/CoursDuSoirIOS-Instruments/CDSProject.m
@@ -1,7 +1,29 @@
 #import "CDSProject.h"
 
+#define kCDSMemoryDummySize 1048576
+
+@interface CDSProject() {
+    unsigned char *dummyBuffer;
+}
+
+@end
+
 @implementation CDSProject
 
 @synthesize identifier, name;
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        dummyBuffer = malloc(kCDSMemoryDummySize);
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    free(dummyBuffer);
+}
 
 @end
